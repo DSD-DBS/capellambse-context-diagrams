81bc52eb5291a63f5398c35191d16e7a17e81e84
ewuerger <ernst.wuerger@gmail.com>
2023-10-16 15:05:09 +0200

    test(class-tree): Add tests

---
+++ tests/test_class_tree_diagrams.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2022 Copyright DB Netz AG and the capellambse-context-diagrams contributors
+# SPDX-License-Identifier: Apache-2.0
+
+import capellambse
+import pytest
+
+CLASS_UUID = "b7c7f442-377f-492c-90bf-331e66988bda"
+
+
+@pytest.mark.parametrize("fmt", ["svgdiagram", "svg", None])
+def test_class_tree_diagram_gets_rendered_successfully(
+    model: capellambse.MelodyModel, fmt: str
+) -> None:
+    obj = model.by_uuid(CLASS_UUID)
+
+    diag = obj.class_tree_diagram
+
+    assert diag.render(fmt)
+
+
+def test_class_tree_diagram_renders_with_additional_params(
+    model: capellambse.MelodyModel,
+) -> None:
+    obj = model.by_uuid(CLASS_UUID)
+
+    diag = obj.class_tree_diagram
+
+    assert diag.render("svgdiagram", edgeRouting="POLYLINE", direction="RIGHT")

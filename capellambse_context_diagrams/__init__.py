79c0cf496557f7920f2128773e4d033d2b61f0c4
Martin Lehmann <martin.lehmann@deutschebahn.com>
2024-05-13 18:21:05 +0200

    feat: Add a helper to install elk.js ahead of time (#97)

--- capellambse_context_diagrams/__init__.py
+++ capellambse_context_diagrams/__init__.py
@@ -29,7 +29,7 @@ from capellambse.model.crosslayer import fa, information
 from capellambse.model.layers import ctx, la, oa, pa
 from capellambse.model.modeltypes import DiagramType
 
-from . import context, styling
+from . import _elkjs, context, styling
 
 try:
     __version__ = metadata.version("capellambse-context-diagrams")
@@ -44,6 +44,18 @@ SupportedClass = tuple[
 logger = logging.getLogger(__name__)
 
 ATTR_NAME = "context_diagram"
+
+
+def install_elk() -> None:
+    """Install elk.js and its dependencies into the local cache directory.
+
+    When rendering a context diagram, elk.js will be installed
+    automatically into a persistent local cache directory. This function
+    may be called while building a container, starting a server or
+    similar tasks in order to prepare the elk.js execution environment
+    ahead of time.
+    """
+    _elkjs._install_required_npm_pkg_versions()
 
 
 def init() -> None:

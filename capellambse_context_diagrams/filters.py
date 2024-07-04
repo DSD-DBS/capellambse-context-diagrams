8496853608ab52b88d12702a080ddd367cd9fec1
Martin Lehmann <martin.lehmann@deutschebahn.com>
2024-07-04 17:09:49 +0200

    feat(filters): Extend (and rename) FEX_EX_ITEMS and FEX_OR_EX_ITEMS
    Rename FEX_EX_ITEMS to SHOW_EX_ITEMS and FEX_OR_EX_ITEMS to
    EXCH_OR_EX_ITEMS, and extend them to work both with FunctionalExchange
    and ComponentExchange.
    
    Co-authored-by: Ernst Würger <ernst.wuerger@deutschebahn.com>

--- capellambse_context_diagrams/filters.py
+++ capellambse_context_diagrams/filters.py
@@ -4,25 +4,29 @@
 from __future__ import annotations
 
 import collections.abc as cabc
-import importlib
 import logging
 import re
 import typing as t
 
-from capellambse.model import common
+from capellambse.model import common, fa
 
-FEX_EX_ITEMS = "show.functional.exchanges.exchange.items.filter"
-"""Show the name of `FunctionalExchange` and its `ExchangeItems` wrapped in
-[E1,...] and separated by ',' - filter in Capella.
+SHOW_EX_ITEMS = "show.functional.exchanges.exchange.items.filter"
+"""Show the name of `ComponentExchange` or `FunctionalExchange` and its
+`ExchangeItems` wrapped in [E1,...] and separated by ',' - filter in Capella.
 """
+FEX_EX_ITEMS = SHOW_EX_ITEMS  # legacy name
 EX_ITEMS = "show.exchange.items.filter"
 """Show `ExchangeItems` wrapped in [E1,...] and separated by ',' - filter
 in Capella.
 """
-FEX_OR_EX_ITEMS = "capellambse_context_diagrams-show.functional.exchanges.or.exchange.items.filter"
-"""Show either `FunctionalExchange` name or its `ExchangeItems` wrapped in
-[E1,...] and separated by ',' - Custom filter, not available in Capella.
+EXCH_OR_EX_ITEMS = (
+    "capellambse_context_diagrams-show.exchanges.or.exchange.items.filter"
+)
+"""Show either the name of the `ComponentExchange` or `FunctionalExchange`, or
+its `ExchangeItems` wrapped in [E1,...] and separated by ',' - Custom filter,
+not available in Capella.
 """
+FEX_OR_EX_ITEMS = EXCH_OR_EX_ITEMS  # legacy name
 NO_UUID = "capellambse_context_diagrams-hide.uuids.filter"
 """Filter out UUIDs from label text."""
 SYSTEM_EX_RELABEL = (
@@ -54,10 +58,10 @@ def exchange_items(obj: common.GenericElement) -> str:
     """Return `obj`'s `ExchangeItem`s wrapped in [E1,...] and separated
     by ','.
     """
-    stringifier = importlib.import_module(
-        "capellambse.aird._filters.global"
-    )._stringify_exchange_items
-    return stringifier(obj, obj._model._loader)
+    assert isinstance(obj, (fa.FunctionalExchange, fa.ComponentExchange))
+    if items := ", ".join(item.name for item in obj.exchange_items):
+        return f"[{items}]"
+    return ""
 
 
 def exchange_name_and_items(
@@ -91,8 +95,8 @@ FILTER_LABEL_ADJUSTERS: dict[
     str, cabc.Callable[[common.GenericElement, str | None], str]
 ] = {
     EX_ITEMS: lambda obj, _: exchange_items(obj),
-    FEX_EX_ITEMS: exchange_name_and_items,
-    FEX_OR_EX_ITEMS: lambda obj, label: (
+    SHOW_EX_ITEMS: exchange_name_and_items,
+    EXCH_OR_EX_ITEMS: lambda obj, label: (
         exchange_items(obj)
         if getattr(obj, "exchange_items", "")
         else label or obj.name
@@ -109,12 +113,10 @@ def sort_exchange_items_label(
     adjustments: dict[str, t.Any],
 ) -> None:
     """Sort the exchange items in the exchange label if value is true."""
-    global_filters = importlib.import_module(
-        "capellambse.aird._filters.global"
-    )
-    adjustments["labels_text"] = global_filters._stringify_exchange_items(
-        exchange, exchange._model._loader, value
-    )
+    items = [item.name for item in exchange.exchange_items]
+    if value:
+        items = sorted(items)
+    adjustments["labels_text"] = ", ".join(items)
 
 
 RENDER_ADJUSTERS: dict[

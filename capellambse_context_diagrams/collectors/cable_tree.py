bb6d60f3be10e78d36a09dea93e787623b42d833
huyenngn <ngan-huyen.thi-nguyen@deutschebahn.com>
2024-09-25 11:40:54 +0200

    docs(cable-tree): Add docs

--- capellambse_context_diagrams/collectors/cable_tree.py
+++ capellambse_context_diagrams/collectors/cable_tree.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2022 Copyright DB InfraGO AG and the capellambse-context-diagrams contributors
 # SPDX-License-Identifier: Apache-2.0
 
+"""This module defines the collector for the CableTreeDiagram."""
 from __future__ import annotations
 
 import copy
@@ -25,6 +26,7 @@ class CableTreeCollector:
         params: dict[str, t.Any],
     ) -> None:
         self.diagram = diagram
+        self.diagram._port_label_position = "OUTSIDE"
         self.obj: m.ModelElement = self.diagram.target
         self.data = makers.make_diagram(diagram)
         self.data.layoutOptions = DEFAULT_LAYOUT_OPTIONS
@@ -36,18 +38,18 @@ class CableTreeCollector:
     def __call__(self) -> _elkjs.ELKInputData:
         src_obj = self.obj.source
         tgt_obj = self.obj.target
-        target_link = self.make_edge(self.obj, src_obj, tgt_obj)
+        target_link = self._make_edge(self.obj, src_obj, tgt_obj)
         target_link.layoutOptions = copy.deepcopy(
             _elkjs.EDGE_STRAIGHTENING_LAYOUT_OPTIONS
         )
-        self.make_tree(src_obj)
-        self.make_tree(tgt_obj, reverse=True)
+        self._make_tree(src_obj)
+        self._make_tree(tgt_obj, reverse=True)
         return self.data
 
-    def make_tree(
+    def _make_tree(
         self, port_obj: m.ModelElement, reverse: bool = False
     ) -> _elkjs.ELKInputChild:
-        box = self.make_port_and_owner(port_obj)
+        box = self._make_port_and_owner(port_obj)
         for link in port_obj.links:
             if self.edges.get(link.uuid):
                 continue
@@ -56,51 +58,45 @@ class CableTreeCollector:
             else:
                 obj = link.source
             if reverse:
-                self.make_edge(link, obj, port_obj)
+                self._make_edge(link, obj, port_obj)
             else:
-                self.make_edge(link, port_obj, obj)
-            self.make_tree(obj, reverse=reverse)
+                self._make_edge(link, port_obj, obj)
+            self._make_tree(obj, reverse=reverse)
         return box
 
-    def make_edge(
+    def _make_edge(
         self,
         link: m.ModelElement,
         src_obj: m.ModelElement,
         tgt_obj: m.ModelElement,
     ) -> _elkjs.ELKInputEdge:
         edge = _elkjs.ELKInputEdge(
-            id=link.uuid, sources=[src_obj.uuid], targets=[tgt_obj.uuid]
+            id=link.uuid,
+            sources=[src_obj.uuid],
+            targets=[tgt_obj.uuid],
+            labels=makers.make_label(
+                link.name,
+            ),
         )
         self.data.edges.append(edge)
         self.edges[link.uuid] = edge
         return edge
 
-    def _make_box(
-        self,
-        obj: t.Any,
-        **kwargs: t.Any,
-    ) -> _elkjs.ELKInputChild:
-        box = makers.make_box(
-            obj,
-            **kwargs,
-        )
-        self.boxes[obj.uuid] = box
-        return box
-
-    def make_port_and_owner(
+    def _make_port_and_owner(
         self, port_obj: m.ModelElement
     ) -> _elkjs.ELKInputChild:
         owner_obj = port_obj.owner
         if not (box := self.boxes.get(owner_obj.uuid)):
-            box = self._make_box(
+            box = makers.make_box(
                 owner_obj,
                 layout_options=makers.DEFAULT_LABEL_LAYOUT_OPTIONS,
             )
+            self.boxes[owner_obj.uuid] = box
             self.data.children.append(box)
         if port := self.ports.get(port_obj.uuid):
             return box
         port = makers.make_port(port_obj.uuid)
-        # port.labels = makers.make_label(port_obj.name)
+        port.labels = makers.make_label(port_obj.name)
         box.ports.append(port)
         self.ports[port_obj.uuid] = port
         return box
@@ -109,4 +105,5 @@ class CableTreeCollector:
 def collector(
     diagram: context.ContextDiagram, params: dict[str, t.Any]
 ) -> _elkjs.ELKInputData:
+    """Main collector function for the CableTreeDiagram."""
     return CableTreeCollector(diagram, params)()

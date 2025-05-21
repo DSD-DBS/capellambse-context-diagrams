15cb9679f726d61a578ae1ac31c8bbda59d07b5e
ewuerger <ernst.wuerger@gmail.com>
2025-05-21 15:47:18 +0200

    test(dataflow): Add more testcases

--- tests/test_dataflow_views.py
+++ tests/test_dataflow_views.py
@@ -17,6 +17,9 @@ from .conftest import (  # type: ignore[import-untyped]
 
 TEST_DATA_FLOW_DATA_ROOT = TEST_ELK_INPUT_ROOT / "data_flow_views"
 TEST_DATA_FLOW_LAYOUT_ROOT = TEST_ELK_LAYOUT_ROOT / "data_flow_views"
+TEST_OCAP_UUID = "3b83b4ba-671a-4de8-9c07-a5c6b1d3c422"
+TEST_CAP_UUID = "9390b7d5-598a-42db-bef8-23677e45ba06"
+TEST_CR_UUID = "72147e11-70df-499b-a339-b81722271f1a"
 RENDER_PARAMS = {
     "display_symbols_as_boxes": True,
     "display_parent_relation": True,
@@ -25,36 +28,40 @@ RENDER_PARAMS = {
 }
 TEST_DATA_FLOW_SET = [
     pytest.param(
-        (
-            "3b83b4ba-671a-4de8-9c07-a5c6b1d3c422",
-            "opcap_data_flow.json",
-            RENDER_PARAMS,
-        ),
+        (TEST_OCAP_UUID, "opcap_data_flow.json", RENDER_PARAMS),
         id="OperationalCapability",
     ),
     pytest.param(
         (
-            "3b83b4ba-671a-4de8-9c07-a5c6b1d3c422",
+            TEST_OCAP_UUID,
             "opcap_without_entities_data_flow.json",
             RENDER_PARAMS | {"display_parent_relation": False},
         ),
         id="OperationalCapability without entities",
     ),
     pytest.param(
-        (
-            "9390b7d5-598a-42db-bef8-23677e45ba06",
-            "cap_data_flow.json",
-            RENDER_PARAMS,
-        ),
+        (TEST_CAP_UUID, "cap_data_flow.json", RENDER_PARAMS),
         id="Capability",
     ),
     pytest.param(
         (
-            "9390b7d5-598a-42db-bef8-23677e45ba06",
+            TEST_CAP_UUID,
             "cap_without_components_data_flow.json",
             RENDER_PARAMS | {"display_parent_relation": False},
         ),
         id="Capability without entities",
+    ),
+    pytest.param(
+        (TEST_CR_UUID, "cap_realization_data_flow.json", RENDER_PARAMS),
+        id="CapabilityRealization",
+    ),
+    pytest.param(
+        (
+            TEST_CR_UUID,
+            "cap_realization_without_components_data_flow.json",
+            RENDER_PARAMS | {"display_parent_relation": False},
+        ),
+        id="CapabilityRealization without entities",
     ),
 ]
 
